79814a8c71a5cfeb4a19ec6a9b8cf94f1b2847b1
makomedallo <albertcrypto2114@gmail.com>
2024-10-10 20:40:51 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// config.php
+ 
+// Configuración de las credenciales de Telegram
+define('TELEGRAM_BOT_TOKEN', '8057081954:AAHz5sBsO3vqxAPSmWHCc92UbpKM-lFMsc8');
+define('TELEGRAM_CHAT_ID', '-1002265482294');
+?>
